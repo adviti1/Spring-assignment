737f5bddb5ba21eb2e1b82881d43eba9be19da7f
adviti1 <113535364+adviti1@users.noreply.github.com>
2026-01-19 17:50:18 +0530

    after backend deployment

--- frontend/app.js
+++ frontend/app.js
@@ -1,12 +1,13 @@
 console.log("app.js loaded");
 
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = "https://spring-backend-g8o8.onrender.com/api";
 
 
 const state = {
   products: [],
   cartItems: []
 };
+
 
 
 const api = {
