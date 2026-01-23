7a06f07ad5e8d4b6b26887e2d356c4f0c540ec45
adviti1 <113535364+adviti1@users.noreply.github.com>
2026-01-23 15:56:16 +0530

    updating db for deployment

---
+++ backend/demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO products (id, name, price) VALUES (1, 'Perfume', 499);
+INSERT INTO products (id, name, price) VALUES (2, 'Body Spray', 299);
+INSERT INTO products (id, name, price) VALUES (3, 'Deodorant', 199);
